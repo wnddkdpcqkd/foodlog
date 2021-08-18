1c26d1f431fd471efc1565cfe11b075a42c713b2
unknown <wnddkdpcqkd@gmail.com>
2021-08-18 17:02:05 +0900

    add store page

--- foodlog/src/Home/addStore.tsx
+++ foodlog/src/Home/addStore.tsx
@@ -9,17 +9,43 @@ export function addStore(props : addStoreProps) {
         return (
             <View style={{flex : 1, backgroundColor : 'white', alignItems : 'center'}}>
                 <View style={{flexDirection : 'row'}}>
-                    <View style={{flex : 1, alignItems: 'center'}}>
+                    <View style={[{flex : 1},styles.center]}>
                         <Text>가게 이름</Text>
                     </View>
                     
-                    <View style={{flex : 2, borderBottomWidth : 1, borderBottomColor : 'black'}}>
+                    <View style={[{flex : 2, },styles.center,styles.textInput]}>
                         <TextInput></TextInput>
                     </View>
+
+                    <View style={{flex : 1}} />
+                </View>
+
+                <View style={{flexDirection : 'row'}}>
+                    <View style={[{flex : 1,},styles.center]}>
+                        <Text>가게 위치</Text>
+                    </View>
                     
-                    
+                    <View style={[{flex : 2, },styles.center,styles.textInput]}>
+                        <TextInput></TextInput>
+                    </View>
+                    <View style={{flex : 1}} />
+
+                </View>
+
+                <View>
+                    <Text>가격</Text>
+                    <Text>맛</Text>
+                    <Text>분위기</Text>
                 </View>
             </View>
         );
 }
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    center : {
+        alignItems : 'center',
+        justifyContent : 'center',
+    },
+    textInput: {
+        borderBottomWidth : 1, borderBottomColor : 'black'
+    }
+})
